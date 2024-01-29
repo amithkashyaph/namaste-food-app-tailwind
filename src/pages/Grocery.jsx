6de4877b92785f4ea35a66c879c0b40fa4acaeb9
amithkashyap <amithkashyaprv@gmail.com>
2024-01-29 23:53:35 +0530

    added Grocery page for routing testing

---
+++ src/pages/Grocery.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Grocery = () => {
+  return (
+    <div>
+      <h1>
+        Grocery webpage. This is a page with a lot of components. This is being
+        used for demonstarting the chunking or bundling startegy for
+        optimisation
+      </h1>
+    </div>
+  );
+};
+
+export default Grocery;
