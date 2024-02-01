193c269e3c58c69c5e23c4759d14174536915e67
amithkashyap <amithkashyaprv@gmail.com>
2024-02-01 13:33:06 +0530

    added About page

---
+++ src/pages/About.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import User from "../components/User";
+import UserClass from "../components/UserClass";
+
+const About = () => {
+  return (
+    <div>
+      <h2>About us</h2>
+      <h3>I am fullstack developer trying to learn React in depth</h3>
+      <UserClass name="Deepthi" />
+    </div>
+  );
+};
+
+export default About;
